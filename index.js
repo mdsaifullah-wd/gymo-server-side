2818b9e9265b08a90c366d644ba59411ec1585e6
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-05-04 15:28:15 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pgx6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    client.connect();
+
+    const inventoryCollection = client.db('inventory').collection('items');
+
+    app.get('/inventory', async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+};
+
+run().catch(console.error);
 
 app.get('/', (req, res) => {
   res.send('Test');
